df2b0ba8e25e3537e433a61e04e7312a382d0a98
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-02-05 18:29:47 -0800

    Coding Exercise 1: Speed Converter (formatting)

--- test/com/ionafan2/jtcjdc/s4/SpeedConverterTest.java
+++ test/com/ionafan2/jtcjdc/s4/SpeedConverterTest.java
@@ -7,11 +7,12 @@ class SpeedConverterTest {
 
     /**
      * Examples of input/output:
-     * toMilesPerHour(1.5); → should return value 1
-     * toMilesPerHour(10.25); → should return value 6
-     * toMilesPerHour(-5.6); → should return value -1
-     * toMilesPerHour(25.42); → should return value 16
-     * toMilesPerHour(75.114); → should return value 47
+     * <br>
+     * <li>toMilesPerHour(1.5); → should return value 1
+     * <li>toMilesPerHour(10.25); → should return value 6
+     * <li>toMilesPerHour(-5.6); → should return value -1
+     * <li>toMilesPerHour(25.42); → should return value 16
+     * <li>toMilesPerHour(75.114); → should return value 47
      */
     @Test
     void toMilesPerHour() {
