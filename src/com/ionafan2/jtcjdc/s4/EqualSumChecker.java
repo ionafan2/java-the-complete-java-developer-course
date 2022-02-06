690f0440024c07fae6a4563eea20cb835ebd9a6d
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-02-05 18:38:47 -0800

    Coding Exercise 6: Equal Sum Checker

---
+++ src/com/ionafan2/jtcjdc/s4/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package com.ionafan2.jtcjdc.s4;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int a, int b, int c) {
+        return (a + b) == c;
+    }
+}
---
+++ test/com/ionafan2/jtcjdc/s4/EqualSumCheckerTest.java
@@ -0,0 +1,23 @@
+package com.ionafan2.jtcjdc.s4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EqualSumCheckerTest {
+
+    /**
+     * Examples of input/output:
+     * <br>
+     * <li>hasEqualSum(1, 1, 1);  should return false since 1 + 1 is not equal to 1
+     * <li>hasEqualSum(1, 1, 2);  should return true since 1 + 1 is equal to 2
+     * <li>hasEqualSum(1, -1, 0);  should return true since 1 + (-1) is 1 - 1 and is equal to 0
+     */
+    @Test
+    void hasEqualSum() {
+        assertFalse(EqualSumChecker.hasEqualSum(1, 1, 1));
+        assertTrue(EqualSumChecker.hasEqualSum(1, 1, 2));
+        assertTrue(EqualSumChecker.hasEqualSum(1, -1, 0));
+    }
+}
