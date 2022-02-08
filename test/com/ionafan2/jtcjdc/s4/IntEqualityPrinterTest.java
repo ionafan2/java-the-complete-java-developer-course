34f6168e0981eef028594485c49e906626ef3f5f
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-02-07 22:10:35 -0800

    Refactor:Coding Exercise 10: Equality Printer Test

--- test/com/ionafan2/jtcjdc/s4/IntEqualityPrinterTest.java
+++ test/com/ionafan2/jtcjdc/s4/IntEqualityPrinterTest.java
@@ -1,8 +1,11 @@
 package com.ionafan2.jtcjdc.s4;
 
+import com.ionafan2.jtcjdc.TestSysOutput;
 import org.junit.jupiter.api.Test;
 
-class IntEqualityPrinterTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IntEqualityPrinterTest extends TestSysOutput {
 
     /**
      * Examples of input/output:
@@ -14,10 +17,22 @@ class IntEqualityPrinterTest {
      */
     @Test
     void printEqual() {
-        System.out.println("\nIntEqualityPrinter.printEqual - test results:");
+        setUpStreams();
+
         IntEqualityPrinter.printEqual(1, 1, 1);
+        String expected1 = "All numbers are equal\n";
+
         IntEqualityPrinter.printEqual(1, 1, 2);
+        String expected2 = "Neither all are equal or different\n";
+
         IntEqualityPrinter.printEqual(-1, -1, -1);
+        String expected3 = "Invalid Value\n";
+
         IntEqualityPrinter.printEqual(1, 2, 3);
+        String expected4 = "All numbers are different\n";
+
+        assertEquals( expected1 + expected2 + expected3 + expected4, outContent.toString());
+
+        restoreStreams();
     }
 }
