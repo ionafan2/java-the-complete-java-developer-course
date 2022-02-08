c4e8c29b9be80e927481fa81e9570a8a9d34f7bf
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-02-07 22:25:44 -0800

    Refactor: Coding Exercise 12: Number In Word Test

--- test/com/ionafan2/jtcjdc/s5/NumberInWordTest.java
+++ test/com/ionafan2/jtcjdc/s5/NumberInWordTest.java
@@ -1,14 +1,27 @@
 package com.ionafan2.jtcjdc.s5;
 
+import com.ionafan2.jtcjdc.TestSysOutput;
 import org.junit.jupiter.api.Test;
 
-class NumberInWordTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberInWordTest extends TestSysOutput {
 
     @Test
     void printNumberInWord() {
-        System.out.println("\nNumberInWordTest.printNumberInWord - test results:");
+        setUpStreams();
+
         NumberInWord.printNumberInWord(0);
+        String expected1 ="ZERO\n";
+
         NumberInWord.printNumberInWord(1);
+        String expected2 ="ONE\n";
+
         NumberInWord.printNumberInWord(9);
+        String expected3 ="NINE\n";
+
+        assertEquals( expected1 + expected2 + expected3, outContent.toString());
+
+        restoreStreams();
     }
 }
