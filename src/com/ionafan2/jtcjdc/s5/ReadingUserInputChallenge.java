924ea9f957337ea2cf639cf51f55c3135cdea39f
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-03-31 19:24:42 -0700

    74. Reading User Input Challenge

---
+++ src/com/ionafan2/jtcjdc/s5/ReadingUserInputChallenge.java
@@ -0,0 +1,35 @@
+package com.ionafan2.jtcjdc.s5;
+
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+
+    public static void main(String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 1;
+        long sum = 0;
+
+        while (counter <= 10) {
+
+            System.out.println("Enter Number #"+counter+":");
+
+            if(scanner.hasNextInt()) {
+                sum += scanner.nextInt();
+            } else {
+                System.out.println("Invalid Number");
+                counter--;
+            }
+            scanner.nextLine();
+
+            counter++;
+        }
+
+        System.out.println("Sum of all numbers is: " + sum);
+
+        scanner.close();
+
+
+    }
+}
